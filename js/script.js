cbeec661d8fc0ef486bf5e80e7702a0774adf54b
Rizkyaditiya21 <rizkyaditiyafernando@gmail.com>
2026-02-13 16:34:09 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,15 @@
 
+let cart = [];
+
+function addToCart(product){
+cart.push(product);
+alert(product + " added to cart");
+}
+
+function checkout(){
+let message = "Order Benz Fragrance:%0A";
+cart.forEach(item=>{
+message += "- " + item + "%0A";
+});
+window.open("https://wa.me/62882000111956?text=" + message);
+}
